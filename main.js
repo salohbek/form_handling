7532f8cca7d62c029e0aab8fa9e16166574c197f
salohbek <saloheshmirzayev07@gmail.com>
2021-06-10 17:08:49 +0500

    finish commit

--- main.js
+++ main.js
@@ -3,8 +3,37 @@ const InputForm={
     <div class="input-form">
   <form @submit='submitForm' class='ui form'>
     <div class="field">
-      <input v-model="newItem" placeholder="Add an Item!" type="text">
+    <label>New Item</label>
+      <input v-model="fields.newItem" placeholder="Add an Item!" type="text">
+  <span style="color:red">{{fieldErrors.newItem}}</span>
+      
     </div>
+
+    <div class="field">
+    <label>Email</label>
+      <input v-model="fields.email" placeholder="What's your email ?" type="text">
+  <span style="color:red">{{fieldErrors.email}}</span>
+
+    </div>
+
+    <div class="field">
+    <label>Urgency</label>
+      <select v-model="fields.urgency" class="ui fluid search dropdown" type="text">
+      <option disabled value="">Please select one</option>
+      <option >Nonessential</option>
+      <option >Moderate</option>
+      <option >Urgent</option>
+      </select>
+  <span style="color:red">{{fieldErrors.urgency}}</span>
+
+    </div>
+    <div class="field">
+    <div class="ui checkbox">
+      <input v-model="fields.termsAndConditions" type="checkbox"/>
+      <label >I accept the Terms and Conditions</label>
+      <span style="color:red">{{fieldErrors.termsAndConditions}}</span>
+    </div>
+  </div>
     <button class="ui button">Submit</button>
   </form>
   
@@ -18,17 +47,60 @@ const InputForm={
     `,
     data(){
         return{
+            fields:{
             newItem:'',
-            items:[]
+            email:'',
+            urgency:'',
+            termsAndConditions:false
+            
+        
+        },
+        fieldErrors:{
+            newItem:undefined,
+            email:undefined,
+            urgency:undefined,
+            termsAndConditions:undefined
         }
+        ,
+
+    
+        items:[],
+    }
     },
     methods:{
        submitForm(evt){
-           this.items.push(this.newItem)
-           this.newItem=''
            evt.preventDefault();
-       }
-
+           this.fieldErrors=this.validateForm(this.fields)
+           if(Object.keys(this.fieldErrors).length) return;
+           this.items.push(this.fields.newItem)
+           this.fields.newItem=''
+           this.fields.email=''
+           this.fields.urgency=''
+           this.fields.termsAndConditions=false
+       },
+       validateForm(fields){
+           const errors={}
+               if(!fields.newItem){
+                   errors.newItem='New Item Required'
+               }
+               if(!fields.email){
+                errors.email='Email Required'
+            }
+            if(!fields.urgency){
+                errors.urgency='Urgency Required'
+            }
+            if(!fields.termsAndConditions){
+                errors.termsAndConditions='Terms and conditions have to be appoved'
+            }
+            if (fields.email && !this.isEmail(fields.email)) {
+                errors.email = "Invalid Email";
+                }
+            return errors
+           
+       },
+       isEmail(email) {
+        const re = /\S+@\S+\.\S+/;
+        return re.test(email);}
 
        // shift+alt+f
      
